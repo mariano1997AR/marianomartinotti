44680239f10480e6755179ef9a0878dacc3108fa
mariano1997AR <93229953+mariano1997AR@users.noreply.github.com>
2023-08-02 11:11:16 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="es-AR">
 <head>
-   <?php include "./static/head.php" ?>
+   <?php include "./static/head.php"; ?>
 </head>
 <body>
-   <?php include "./static/headers.php" ?>
+   <?php include "./static/headers.php"; ?>
    <script src="./js/app.js"></script>
 </body>
 </html>
